2229ed531bd783a1480960053b23ffcdf188eb53
Lalatianalinda <147044310+Lalatianalinda@users.noreply.github.com>
2023-10-27 19:28:50 +0200

    Update Robot.java
    completed veille remain

--- src/tamagotchi/Robot.java
+++ src/tamagotchi/Robot.java
@@ -1,17 +1,22 @@
 package tamagotchi;
 
-public class Robot {
+public class Robot extends Tamagotchi{
   private int batterie;
   public void seRecharger(int n){
-    
+    if (getBatterie() + n <= 100) {
+            setBatterie(getBatterie() + n);
+        }
+        else {
+            setBatterie(100);
+        }
   }
   public void enVeille(int n){
-    
+    //TODO
   }
   public int getBatterie(int n){
-    
+    return batterie;
   }
   public void setBatterie(){
-    
+    batterie = n;
   }
 }
