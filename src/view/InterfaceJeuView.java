6eb9b142104392ad9be16e2b524386e7ebfaad45
Lalatianalinda <lalatianalinda@gmail.com>
2023-12-21 12:20:13 +0100

    Mise à jour de InterfaceJeuVeiw

--- src/view/InterfaceJeuView.java
+++ src/view/InterfaceJeuView.java
@@ -8,30 +8,48 @@ import javax.imageio.ImageIO;
 public class InterfaceJeuView extends JPanel {
 
     private Image backgroundImage;
+    private JButton bouton1;
+    private JButton bouton2;
+    private JButton bouton3;
+    private JButton bouton4;
+    private JButton bouton5;
+    private JPanel boutonPanel;
 
     public InterfaceJeuView(View frame) {
+        // Configuration du layout du panneau principal
+        setLayout(new BorderLayout());
 
-        // Ajout du panneau d'arrière-plan
+        // Charger l'image d'arrière-plan
         try {
-        	backgroundImage = ImageIO.read("../media/salon.png");
-            
+            backgroundImage = ImageIO.read(getClass().getResource("/media/salon.png"));
         } catch (IOException e) {
             e.printStackTrace();
             System.exit(1);
         }
-        
-        
+
+        // Initialisation des boutons
+        bouton1 = new JButton("Salon");
+        bouton2 = new JButton("Salle de bain");
+        bouton3 = new JButton("Chambre");
+        bouton4 = new JButton("Cuisine");
+        bouton5 = new JButton("Jardin");
+
+        // Initialisation du panneau de boutons
+        boutonPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        boutonPanel.add(bouton1);
+        boutonPanel.add(bouton2);
+        boutonPanel.add(bouton3);
+        boutonPanel.add(bouton4);
+        boutonPanel.add(bouton5);
+
+        // Ajouter le panneau de boutons en bas de la fenêtre
+        add(boutonPanel, BorderLayout.SOUTH);
     }
+
+    @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        // l'image en arrière-plan
+        // Dessiner l'image en arrière-plan
         g.drawImage(backgroundImage, 0, 0, this.getWidth(), this.getHeight(), this);
-    }
-
- 
-
-    public static void main(String[] args) {
-        InterfaceJeuView frame = new InterfaceJeuView();
-        frame.setVisible(true);
     }
 }
