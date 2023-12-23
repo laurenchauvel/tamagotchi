109aa4717cdde4864538bace6f060092bd205711
Lalatianalinda <lalatianalinda@gmail.com>
2023-12-23 18:07:42 +0100

    Mise a jour SauvegardeView

--- src/view/SauvegardesView.java
+++ src/view/SauvegardesView.java
@@ -1,68 +1,114 @@
 package view;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 
 import controller.Controller;
-import controller.Sauvegarde;
-
 import java.util.List;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import controller.Sauvegarde;
 import modele.Tamagotchi;
+
 
 @SuppressWarnings("serial")
 public class SauvegardesView extends JPanel {
     private Sauvegarde sauvegarde;
-    
-    private Controller controller ;
-    
-    private JButton retour ;
+    private Controller controller;
+    private JPanel centerPanel;
+    private JButton retour;
+    private Image backgroundImage;
 
     public SauvegardesView(View frame) {
-        
-        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-        
-        retour = new JButton("Retour");
-        retour.addActionListener(e -> frame.getLayout().show(frame.getPanel(), frame.getMenu()));
-        this.add(retour);
-    }
-    
-    public void showSavedGames() {
-        // Effacer les boutons précédents (si nécessaire)
-        this.removeAll();
+    	try {
+            backgroundImage = ImageIO.read(getClass().getResource("/media/maison.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.err.println("Erreur de chargement de l'image de fond : " + e.getMessage());
+        }
+        setLayout(new BorderLayout());
 
-        // Ajouter à nouveau le bouton de retour
-        this.add(retour);
+        // Style du bouton retour
+        retour = new JButton("Retour");
+        retour.setBackground(new Color(216, 20, 20));
+        JPanel retourPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        retourPanel.setOpaque(false);
+        retourPanel.add(retour);
+        this.add(retourPanel, BorderLayout.NORTH);
+
+        retour.addActionListener(e -> frame.getLayout().show(frame.getPanel(), frame.getMenu()));
+
+        // Panel central pour les boutons de sauvegarde
+        centerPanel = new JPanel(new FlowLayout(FlowLayout.CENTER, 10, 10));
+        centerPanel.setOpaque(false);
+        this.add(centerPanel, BorderLayout.CENTER);
+      
+    }
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        if (backgroundImage != null) {
+            g.drawImage(backgroundImage, 0, 0, this.getWidth(), this.getHeight(), this);
+        }
+    }
+
+
+    public void showSavedGames() {
+        centerPanel.removeAll();
 
         List<Tamagotchi> savedGames = controller.getTamagoSauvegardes();
         if (savedGames == null || savedGames.isEmpty()) {
-            this.add(new JLabel("Aucun Tamagotchi sauvegardé pour l'instant."));
+            JLabel noSavedLabel = new JLabel("Aucun Tamagotchi sauvegardé pour l'instant.");
+            noSavedLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+            centerPanel.add(noSavedLabel);
         } else {
             for (Tamagotchi tamagotchi : savedGames) {
-                String buttonText = tamagotchi.getNom() + ":" + tamagotchi.getEspece();
-                JButton button = new JButton(buttonText);
-                button.addActionListener(new ActionListener() {
-                    @Override
-                    public void actionPerformed(ActionEvent e) {
-                        // TODO: Action à effectuer lorsqu'un bouton est cliqué
-                    }
+                String buttonText = tamagotchi.getNom() + " : " + tamagotchi.getEspece();
+                GradientButton button = new GradientButton(buttonText);
+                button.setPreferredSize(new Dimension(150, 30)); // Même dimension pour tous les boutons
+
+                button.addActionListener(e -> {
+                    Options(tamagotchi);
                 });
-                add(button);
+
+                centerPanel.add(button);
             }
         }
 
-        // Rafraîchir l'affichage du panel
-        this.revalidate();
-        this.repaint();
+        // Rafraîchir l'affichage du panel central
+        centerPanel.revalidate();
+        centerPanel.repaint();
     }
     
+    private void Options(Tamagotchi tamagotchi) {
+        // Options disponibles
+        String[] options = {"Jouer", "Supprimer", "Retour"};
+        int response = JOptionPane.showOptionDialog(this, 
+            "Que voulez-vous faire avec ce Tamagotchi ?", 
+            "Options pour " + tamagotchi.getNom(),
+            JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE,
+            null, options, options[0]);
+
+        switch (response) {
+            case 0: // Jouer
+                
+                break;
+            case 1: // Supprimer
+                
+                break;
+            case 2: // Retour
+                // Ne rien faire, retourner à l'écran précédent
+                break;
+        }
+    }
     public void setSauvegarde(Sauvegarde sauvegarde) {
-		this.sauvegarde = sauvegarde;
-	}
-    
+        this.sauvegarde = sauvegarde;
+    }
+
     public void setController(Controller controller) {
-		this.controller = controller;
-	}
+        this.controller = controller;
+    }
 }
